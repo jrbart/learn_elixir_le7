48396d075779d8e7c5aa28413533828c5d1b2d4c
jrbart <jorabart@gmail.com>
2026-07-13 18:14:05 -0500

    timestamps don't need primary key

--- graphql_api/lib/graphql_api/accounts/auth_pipe.ex
+++ graphql_api/lib/graphql_api/accounts/auth_pipe.ex
@@ -1,16 +1,9 @@
 defmodule GraphqlApi.Accounts.Timestamps do
   use Ecto.Schema
-  # import Ecto.Changeset
+  @primary_key false
 
   schema "timestamps" do
     field :timestamp, :utc_datetime
   end
 
-  # @doc false
-  # def changeset(user_token, attrs) do
-  #   user_token
-  #   |> cast(attrs, [:user_id, :token])
-  #   |> validate_required([:user_id, :token])
-  #   |> foreign_key_constraint(:user_id)
-  # end
 end
--- graphql_api/priv/repo/migrations/20260713220637_add_table_authpipe.exs
+++ graphql_api/priv/repo/migrations/20260713220637_add_table_authpipe.exs
@@ -2,7 +2,7 @@ defmodule GraphqlApi.Repo.Migrations.AddTableAuthpipe do
   use Ecto.Migration
 
   def change do
-    create table(:timestamps) do
+    create table(:timestamps, primary_key: false) do
       add :timestamp, :utc_datetime 
     end
 
