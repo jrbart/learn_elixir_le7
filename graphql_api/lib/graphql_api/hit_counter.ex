a40df20b8dc2522bdbeef087f98d9eb20742ae42
jrbart <jorabart@gmail.com>
2026-07-23 12:11:05 -0500

    change logging level on hit counter

--- graphql_api/lib/graphql_api/hit_counter.ex
+++ graphql_api/lib/graphql_api/hit_counter.ex
@@ -6,12 +6,12 @@ defmodule GraphqlApi.HitCounter do
   end
 
   def value(key) do
-    SharedUtils.Logger.info(__MODULE__, "GET #{key}")
+    SharedUtils.Logger.debug(__MODULE__, "GET #{key}")
     Agent.get(__MODULE__, &Keyword.get(&1, key, 0))
   end
 
   def increment(key) do
-    SharedUtils.Logger.info(__MODULE__, "INC #{key} #{value(key)}")
+    SharedUtils.Logger.debug(__MODULE__, "INC #{key} #{value(key)}")
     Agent.update(__MODULE__, &Keyword.update(&1, key, 1, fn v -> v + 1 end))
   end
 end
